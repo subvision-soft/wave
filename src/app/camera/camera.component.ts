627ea911eba38920e27000943a3c14b6a775a976
Paul <paulcoignac@gmail.com>
2023-05-20 14:55:26 +0200

    Amélioration de l'expérience utilisateur en affichant l'aperçu de la caméra au lieu de chaques frame traitées

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -3,6 +3,7 @@ import { NgxOpenCVService } from '../../lib/ngx-open-cv.service';
 import { OpenCVState } from '../../lib/models';
 import { DOCUMENT } from '@angular/common';
 import { Router } from '@angular/router';
+import { PlastronService } from '../plastron.service';
 
 @Component({
   selector: 'app-camera',
@@ -17,13 +18,39 @@ export class CameraComponent {
 
   height: number = 0;
   width: number = 0;
+  currentFps: number = 0;
+  coordinates: any = null;
+  coordinatesPercent: any = null;
+
+  coordinatesToPercent(coordinates: any) {
+    if (!coordinates) {
+      return;
+    }
+    this.coordinatesPercent = coordinates.map((coordinate: any) => {
+      return {
+        x: (coordinate.x / this.width) * 100,
+        y: (coordinate.y / this.height) * 100,
+      };
+    });
+  }
+  getPolygon() {
+    let result = '';
+    if (!this.coordinatesPercent) {
+      return;
+    }
+    for (const coordinate of this.coordinatesPercent) {
+      result += `${coordinate.x}% ${coordinate.y}%,`;
+    }
+    return `polygon(${result.slice(0, -1)})`;
+  }
 
   getFrameFromStream() {}
 
   constructor(
     private ngxOpenCv: NgxOpenCVService,
     @Inject(DOCUMENT) document: Document,
-    private router: Router
+    private router: Router,
+    private plastronService: PlastronService
   ) {
     router.events.subscribe((val) => {
       this.playing = false;
@@ -37,13 +64,14 @@ export class CameraComponent {
       })
       .then(function (stream: MediaStream) {
         let video = document.getElementById('video');
-        // @ts-ignore
-        video.srcObject = stream;
-        // @ts-ignore
-        video.play().then((test) => {
-          console.log('test');
-          scope.initOpencv();
-        });
+        if (video) {
+          // @ts-ignore
+          video.srcObject = stream;
+          // @ts-ignore
+          video.play().then((test) => {
+            scope.initOpencv();
+          });
+        }
       })
       .catch(function (err) {
         console.log('An error occurred! ' + err);
@@ -70,214 +98,10 @@ export class CameraComponent {
     });
   }
 
-  // @ts-ignore
-  getDistance(p1, p2) {
-    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
-  }
-
-  // @ts-ignore
-  orderPoints(pts) {
-    let rect = new Array(4);
-    let pts2 = [...pts];
-
-    pts2.sort((a, b) => a.x + a.y - (b.x + b.y));
-    rect[0] = pts2[0];
-    rect[2] = pts2[pts2.length - 1];
-
-    pts2.sort((a, b) => a.x - a.y - (b.x - b.y));
-    rect[3] = pts2[0];
-    rect[1] = pts2[pts2.length - 1];
-
-    return rect;
-  }
-
-  // @ts-ignore
-  extractDocument(mat) {
-    // @ts-ignore
-    const cv = window['cv'];
-    let img = mat.clone();
-    let kernel = new cv.Mat.ones(5, 5, cv.CV_8UC1);
-    cv.morphologyEx(img, img, cv.MORPH_CLOSE, kernel, new cv.Point(-1, -1), 3);
-
-    let gray = new cv.Mat();
-    cv.cvtColor(img, gray, cv.COLOR_BGR2GRAY);
-    img.delete();
-    let blurred = new cv.Mat();
-    cv.GaussianBlur(gray, blurred, new cv.Size(131, 131), 50);
-    blurred.delete();
-    let edged = new cv.Mat();
-    cv.Canny(gray, edged, 100, 200);
-    gray.delete();
-
-    cv.dilate(edged, edged, kernel, new cv.Point(-1, -1), 4);
-    cv.erode(edged, edged, kernel, new cv.Point(-1, -1), 4);
-    kernel.delete();
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    cv.findContours(
-      edged,
-      contours,
-      hierarchy,
-      cv.RETR_EXTERNAL,
-      cv.CHAIN_APPROX_SIMPLE
-    );
-    hierarchy.delete();
-    edged.delete();
-
-    let contoursArray = [];
-    for (let i = 0; i < contours.size(); i++) {
-      contoursArray.push(contours.get(i));
-    }
-
-    contoursArray.sort(
-      (contour1, contour2) =>
-        cv.contourArea(contour2) - cv.contourArea(contour1)
-    );
-
-    let screenCnt = null;
-    for (let contour of contoursArray) {
-      let approxDistance = cv.arcLength(contour, true) * 0.02;
-      let approx = new cv.Mat();
-      cv.approxPolyDP(contour, approx, approxDistance, true);
-
-      let valid = true;
-
-      if (approx.total() === 4) {
-        let angles = [];
-        for (let i = 0; i < 4; i++) {
-          let p1 = {
-            x: approx.data32F[i * 2],
-            y: approx.data32F[i * 2 + 1],
-          };
-          let p2 = {
-            x: approx.data32F[((i + 1) % 4) * 2],
-            y: approx.data32F[((i + 1) % 4) * 2 + 1],
-          };
-          let p3 = {
-            x: approx.data32F[((i + 2) % 4) * 2],
-            y: approx.data32F[((i + 2) % 4) * 2 + 1],
-          };
-          let dx1 = p1.x - p2.x;
-          let dy1 = p1.y - p2.y;
-          let dx2 = p3.x - p2.x;
-          let dy2 = p3.y - p2.y;
-          let angle = Math.abs(
-            (Math.atan2(dx1 * dy2 - dy1 * dx2, dx1 * dx2 + dy1 * dy2) * 180) /
-              Math.PI
-          );
-          angles.push(angle);
-        }
-
-        for (let angle of angles) {
-          if (angle < 70 || angle > 110) {
-            valid = false;
-            break;
-          }
-        }
-
-        let area = cv.contourArea(approx);
-        let ratio = area / (mat.cols * mat.rows);
-        if (ratio < 0.1) {
-          valid = false;
-        }
-
-        if (valid) {
-          screenCnt = approx;
-          break;
-        }
-      }
-    }
-
-    if (screenCnt === null) {
-      throw new Error('Aucun plastron trouvé');
-    }
-    for (let i = 0; i < screenCnt.total(); i++) {
-      let p1 = new cv.Point(
-        screenCnt.data32S[i * 2],
-        screenCnt.data32S[i * 2 + 1]
-      );
-      let p2 = new cv.Point(
-        screenCnt.data32S[((i + 1) % screenCnt.total()) * 2],
-        screenCnt.data32S[((i + 1) % screenCnt.total()) * 2 + 1]
-      );
-      cv.line(mat, p1, p2, [0, 255, 0, 255], 2);
-    }
-
-    // cv.drawContours(mat, polyLines, -1, [0, 0, 255, 255], 3);
-    return mat;
-
-    let topLeft = { x: screenCnt.data32S[0], y: screenCnt.data32S[1] };
-    let topLeftIndex = 0;
-    let minDistance = this.getDistance(topLeft, new cv.Point(0, 0));
-
-    for (let i = 1; i < screenCnt.total(); i++) {
-      let distance = this.getDistance(
-        { x: screenCnt.data32S[i * 2], y: screenCnt.data32S[i * 2 + 1] },
-        new cv.Point(0, 0)
-      );
-      if (distance < minDistance) {
-        minDistance = distance;
-        topLeftIndex = i;
-      }
-    }
-
-    let firstHalf = [];
-    for (let i = 0; i < topLeftIndex; i++) {
-      firstHalf.push({
-        x: screenCnt.data32S[i * 2],
-        y: screenCnt.data32S[i * 2 + 1],
-      });
-    }
-
-    let secondHalf = [];
-    for (let i = topLeftIndex; i < screenCnt.total(); i++) {
-      secondHalf.push({
-        x: screenCnt.data32S[i * 2],
-        y: screenCnt.data32S[i * 2 + 1],
-      });
-    }
-
-    let points = firstHalf.concat(secondHalf);
-
-    let pointsData = new Float32Array(points.length * 2);
-    for (let i = 0; i < points.length; i++) {
-      pointsData[i * 2] = points[i].x;
-      pointsData[i * 2 + 1] = points[i].y;
-    }
-
-    const sourceCoordinates = [
-      [points[0].x, points[0].y],
-      [points[1].x, points[1].y],
-      [points[2].x, points[2].y],
-      [points[3].x, points[3].y],
-    ];
-
-    let approx = cv.matFromArray(4, 1, cv.CV_32FC2, sourceCoordinates.flat());
-
-    const destCoordinates = [
-      [0, 0],
-      [mat.cols - 1, 0],
-      [mat.cols - 1, mat.rows - 1],
-      [0, mat.rows - 1],
-    ];
-
-    const matFromArray = cv.matFromArray(
-      4,
-      1,
-      cv.CV_32FC2,
-      destCoordinates.flat()
-    );
-    let transform = cv.getPerspectiveTransform(approx, matFromArray);
-    let warped = new cv.Mat();
-    cv.warpPerspective(mat, warped, transform, mat.size());
-    console.log(transform);
-    // cv.resize(warped, warped, new cv.Size(1500, 1500));
-    return warped;
-  }
-
   startWebcam() {
     // @ts-ignore
     const cv = window['cv'];
+    this.plastronService.setCv(cv);
     let video = document.getElementById('video');
 
     while (video == null) {
@@ -299,7 +123,18 @@ export class CameraComponent {
     let frame = new cv.Mat(this.height, this.width, cv.CV_8UC4);
     const fps = 60;
     const scope = this;
+
+    let currentFps = 0;
+    let currentTimestamp = new Date().getTime();
     function processVideo() {
+      if (new Date().getTime() - currentTimestamp > 1000) {
+        scope.currentFps = currentFps;
+        currentFps = 0;
+        currentTimestamp = new Date().getTime();
+      } else {
+        currentFps += 1;
+      }
+
       // @ts-ignore
       if (!scope.playing) {
         frame.delete();
@@ -309,19 +144,13 @@ export class CameraComponent {
         try {
           // @ts-ignore
           cap.read(frame);
-          try {
-            cv.imshow('canvas', scope.extractDocument(frame));
-          } catch (err) {
-            console.log(err);
-            cv.imshow('canvas', frame);
-          }
-          // mat.delete();
-        } catch (err) {
-          console.log(err);
-          //print stack trace
-          // @ts-ignore
-          console.log(err.stack);
-        }
+          // cv.imshow('canvas', frame);
+          new Promise((resolve, reject) => {
+            scope.coordinates =
+              scope.plastronService.getPlastronCoordinates(frame);
+            scope.coordinatesToPercent(scope.coordinates);
+          });
+        } catch (err) {}
       } else {
         video = document.getElementById('video');
         // @ts-ignore
