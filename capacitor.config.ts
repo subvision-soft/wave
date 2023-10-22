eaa2da5037196493e61c4c7fa1f94e204120252e
Paul <paulcoignac@gmail.com>
2023-10-22 22:19:13 +0200

    - Changement de logo et de nom

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'fr.subapp.app',
-  appName: 'calypso',
+  appName: 'wave',
   webDir: 'dist/subapp',
   server: {
     androidScheme: 'https',
