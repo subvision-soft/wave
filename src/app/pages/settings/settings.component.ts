7277407983c1b764246e6b08b41431ef6e68745d
Paul <paulcoignac@gmail.com>
2023-10-24 19:37:18 +0200

    Bug fix : settings on the settings page where not up to date if the page was refresh

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -45,81 +45,73 @@ export class SettingsComponent {
     this.parametersService.set(setting.id, setting.value);
   }
 
-  private _settings: ParameterGroup[] = [
-    {
-      label: 'SETTINGS.APPEARANCE.TITLE',
-      parameters: [
-        {
-          id: 'COULEUR_PRINCIPALE',
-          label: 'SETTINGS.APPEARANCE.FIRST_COLOR',
-          value: this.parametersService.get('COULEUR_PRINCIPALE').value,
-          type: 'color',
-        },
-        {
-          id: 'COULEUR_SECONDAIRE',
-          label: 'SETTINGS.APPEARANCE.SECOND_COLOR',
-          value: this.parametersService.get('COULEUR_SECONDAIRE').value,
-          type: 'color',
-        },
-        {
-          id: 'RAYON_BORDURES',
-          label: 'SETTINGS.APPEARANCE.BORDER_RADIUS',
-          value: this.parametersService.get('RAYON_BORDURES').value,
-          type: 'number',
-          min: 0,
-          max: 40,
-        },
-        // {
-        //   id: 'TAILLE_POLICE',
-        //   label: 'Taille de police',
-        //   value: this.parametersService.get('TAILLE_POLICE').value,
-        //   min: 10,
-        //   max: 40,
-        //   type: 'number',
-        // },
-        // {
-        //   id: 'TAILLE_ESPACES',
-        //   label: 'Taille des espacements',
-        //   value: this.parametersService.get('TAILLE_ESPACES').value,
-        //   min: 10,
-        //   max: 40,
-        //   type: 'number',
-        // },
-        {
-          id: 'THEME_SOMBRE',
-          label: 'SETTINGS.APPEARANCE.THEME_DARK',
-          value: this.parametersService.get('THEME_SOMBRE').value,
-          type: 'checkbox',
-        },
-      ],
-    },
-    {
-      label: 'SETTINGS.OTHERS.TITLE',
-      parameters: [
-        {
-          id: 'LANGUE',
-          label: 'Langue',
-          value: this.parametersService.get('LANGUE').value,
-          type: 'select',
-          store: [
-            { id: 'fr', label: 'Français' },
-            { id: 'en', label: 'English' },
-            { id: 'es', label: 'Español' },
-            { id: 'it', label: 'Italiano' },
-            { id: 'ar-KW', label: 'العربية (الكويت)' },
-            { id: 'ar-DZ', label: 'العربية (الجزائر)' },
-          ],
-        },
-      ],
-    },
-  ];
+  private _settings: ParameterGroup[] = [];
+
+  initSettings() {
+    this._settings = [
+      {
+        label: 'SETTINGS.APPEARANCE.TITLE',
+        parameters: [
+          {
+            id: 'COULEUR_PRINCIPALE',
+            label: 'SETTINGS.APPEARANCE.FIRST_COLOR',
+            value: this.parametersService.get('COULEUR_PRINCIPALE').value,
+            type: 'color',
+          },
+          {
+            id: 'COULEUR_SECONDAIRE',
+            label: 'SETTINGS.APPEARANCE.SECOND_COLOR',
+            value: this.parametersService.get('COULEUR_SECONDAIRE').value,
+            type: 'color',
+          },
+          {
+            id: 'RAYON_BORDURES',
+            label: 'SETTINGS.APPEARANCE.BORDER_RADIUS',
+            value: this.parametersService.get('RAYON_BORDURES').value,
+            type: 'number',
+            min: 0,
+            max: 40,
+          },
+          {
+            id: 'THEME_SOMBRE',
+            label: 'SETTINGS.APPEARANCE.THEME_DARK',
+            value: this.parametersService.get('THEME_SOMBRE').value,
+            type: 'checkbox',
+          },
+        ],
+      },
+      {
+        label: 'SETTINGS.OTHERS.TITLE',
+        parameters: [
+          {
+            id: 'LANGUE',
+            label: 'Langue',
+            value: this.parametersService.get('LANGUE').value,
+            type: 'select',
+            store: [
+              { id: 'fr', label: 'Français' },
+              { id: 'en', label: 'English' },
+              { id: 'es', label: 'Español' },
+              { id: 'it', label: 'Italiano' },
+              { id: 'ar-KW', label: 'العربية (الكويت)' },
+              { id: 'ar-DZ', label: 'العربية (الجزائر)' },
+            ],
+          },
+        ],
+      },
+    ];
+  }
 
   constructor(
     private parametersService: ParametersService,
     private filesService: FilesService
   ) {
-    filesService.clearTarget();
-    filesService.clearSession();
+    console.log(this.parametersService.get('LANGUE').value);
+    this.filesService.clearTarget();
+    this.filesService.clearSession();
+    this.parametersService.loaded.subscribe((loaded: boolean) => {
+      this.initSettings();
+    });
   }
 
   protected readonly ParameterType = ParameterType;
